69d3bad6967fde961dd130b65aeb4754b024d503
bmarolleau <benoit.marolleau@fr.ibm.com>
2021-07-01 12:02:44 +0200

    adding oracle config

--- src/main/java/com/acmeair/db2/services/CustomerServiceImplDb2.java
+++ src/main/java/com/acmeair/db2/services/CustomerServiceImplDb2.java
@@ -382,7 +382,7 @@ public class CustomerServiceImplDb2 extends CustomerService implements Db2Consta
 
   @Override
   public String getServiceType() {
-    return "db2 for i";
+    return "backend database";
   }
 
   @Override
